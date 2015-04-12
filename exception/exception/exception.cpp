dabce30fd1c5b5e318f985e3934a19957dfeebf2
Carol <luoshengwen568@gmail.com>
2015-04-12 14:18:58 +0800

    2015-4-12
    2015-4-12

--- exception/exception/exception.cpp
+++ exception/exception/exception.cpp
@@ -2,8 +2,15 @@
 #include "exception.h"
 
 
-int main()
+int main(int argc,char agrv[]) //传参数
 {
+
+	exception ex;
+	if (ex.createDB())
+		std::cout << "yes";
+	else
+		std::cout << "no";
+	system("pause");
 	//Sleep(3000);
 //	BOOL WINAPI FreeConsole(void); // 不显示控制台窗口
 	return 0;
--- exception/exception/exception.h
+++ exception/exception/exception.h
@@ -1,7 +1,91 @@
 #include "sqlite3.h"
 #include <fstream>
 #include <windows.h>
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <errno.h>
+#include <tchar.h>
 
-#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+
+//#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")  //不显示
 #pragma comment(lib,"Shlwapi.lib")
 #pragma comment(lib,"sqlite3.lib")
+
+class exception
+{
+public:
+	exception();
+	~exception(){};
+	bool createDB();
+private:
+	sqlite3 * conn;
+	char * err_msg;
+	char sql[200];
+	bool createData();
+	bool connectdata();
+	bool CreateTable();
+
+};
+
+exception::exception()
+{
+	conn = NULL;
+	err_msg = NULL;
+	memset(sql, 0, sizeof(char) * 200);
+}
+
+//not stop create DB directory and data.db file untill seccussfully
+bool exception::createDB()
+{
+	WIN32_FIND_DATA wfd;
+	HANDLE fhandle = FindFirstFile(_T("./DB"), &wfd);
+	if (fhandle == INVALID_HANDLE_VALUE)
+	{
+		//std::cout << "directory not exsit!";
+		while (!CreateDirectory(_T("./DB"), NULL));
+		while (!createData());
+	}
+	else
+	{
+		//std::cout << "directory exsit!";
+		while (!createData());
+	}
+	
+	return true;
+}
+
+//create data.db file
+bool exception::createData()
+{
+	std::ofstream out;
+	out.open("./DB/data.db", std::ios::out);
+	if (out)
+	{
+		//std::cout << "1";
+		out.close();
+		return true;
+	}
+	else
+	{
+		//std::cout << "2";
+		out.close();
+		return false;
+	}
+	
+}
+
+bool exception::connectdata()
+{
+	if (sqlite3_open("./RGS.db", &conn) != SQLITE_OK)
+	{
+		std::cout << "无法打开数据库，请尝试重启程序或者删除与此程序同一目录下的RSG.db文件！" << std::endl;
+		exit(-1);
+	}
+	else
+	{
+		CreateTable(conn, sql, err_msg);
+		std::cout << "数据空空如也，尝试手动添加数据吧！" << std::endl;
+	}
+	return true;
+}
